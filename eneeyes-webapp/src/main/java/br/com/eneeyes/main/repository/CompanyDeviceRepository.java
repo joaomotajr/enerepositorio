6b59b7c6a692a2b42b6c6949aad924b073668f06
joaom <joaom@DESKTOP-PPE05P1>
2016-08-04 18:58:37 -0300

    company detectors testes

--- eneeyes-webapp/src/main/java/br/com/eneeyes/main/repository/CompanyDeviceRepository.java
+++ eneeyes-webapp/src/main/java/br/com/eneeyes/main/repository/CompanyDeviceRepository.java
@@ -1,9 +1,15 @@
 package br.com.eneeyes.main.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.eneeyes.main.model.CompanyDevice;
 
 public interface CompanyDeviceRepository  extends JpaRepository<CompanyDevice, Long> {
+	
+	@Query("select c from CompanyDevice c where c.area.uid = ?1")
+	public List<CompanyDevice> findCompanyDeviceByIdArea(Long uid);
 	
 }

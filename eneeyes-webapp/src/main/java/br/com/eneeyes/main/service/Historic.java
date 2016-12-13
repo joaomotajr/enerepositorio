6c5ea804576fb7579e7b7a5f68d0e86f65a33902
Joao Junior <joaomotajr@hotmail.com>
2016-12-13 20:09:47 -0200

    Test WS

---
+++ eneeyes-webapp/src/main/java/br/com/eneeyes/main/service/Historic.java
@@ -0,0 +1,30 @@
+//package br.com.eneeyes.main.service;
+//
+//import javax.jws.WebParam;
+//
+////@WebService
+////@Stateless
+//public class Historic implements HistoricWs {
+//		
+//	 public void save (Long CompanyId, String SensorId, Double value) {
+//		 
+//	 }	
+//	 
+//	 public double fazerOp(@WebParam(name = "num1") double a, @WebParam(name = "num2") double b, @WebParam(name = "op") String op) {
+//		
+//		 switch (op) {
+//			case "+":
+//				return a + b;
+//			case "-":
+//				return a - b;
+//			case "*":
+//				return a * b;
+//			case "/":
+//				return a / b;
+//			default:
+//				throw new IllegalArgumentException("OperaÃ§Ã£o '" + op
+//						+ "' nÃ£o reconhecida. Informa '+', '-', '*' ou '/'.");
+//			}
+//		}
+//
+//}

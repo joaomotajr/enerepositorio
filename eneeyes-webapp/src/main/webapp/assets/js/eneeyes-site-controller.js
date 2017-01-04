a67b0cbe9b1f96ad704494c6f985a020a7751a3f
Joao Mota Jr <joaomotajr@hotmail.com>
2017-01-04 10:27:50 -0200

    Revert "Comentando itens não utilizados"
    This reverts commit 8bfc3a12922284c69756d4c09b5a918cd27eb572.

--- eneeyes-webapp/src/main/webapp/assets/js/eneeyes-site-controller.js
+++ eneeyes-webapp/src/main/webapp/assets/js/eneeyes-site-controller.js
@@ -280,139 +280,139 @@ app.controller('SiteController', function ($scope, $http, $filter, $interval, $t
 //        return deferred.promise;
 //    };
 
-//	$scope.clearForgetPasswordForm = function () {
-//		if ($scope.forms.forgetPassword == null) {
-//			return;
-//		}
-//		$scope.forms.forgetPassword.email1 = '';
-//		$scope.forms.forgetPassword.email2 = '';
-//		$scope.forms.forgetPassword.errorMessage = null;
-//		$scope.forms.forgetPassword.successMessage = null;
-//	};
+	$scope.clearForgetPasswordForm = function () {
+		if ($scope.forms.forgetPassword == null) {
+			return;
+		}
+		$scope.forms.forgetPassword.email1 = '';
+		$scope.forms.forgetPassword.email2 = '';
+		$scope.forms.forgetPassword.errorMessage = null;
+		$scope.forms.forgetPassword.successMessage = null;
+	};
 
-//    $scope.recuperarSenha = function() {
-//    	angular.element('body').addClass('loading');
-//    	$scope.forms.forgetPassword.errorMessage = null;
-//        $scope.forms.forgetPassword.successMessage = null;
-//        $http.post('/api/perfil/recover-password?_csrf='+angular.element('#_csrf').val(),
-//			{email1:$scope.forms.forgetPassword.email1, email2:$scope.forms.forgetPassword.email2}).success(function(data) {
-//			angular.element('body').removeClass('loading');
-//			if (data.errorMessages.length > 0) {
-//				$scope.forms.forgetPassword.errorMessage = data.errorMessages[0].message;
-//			}
-//			if (data.successMessages.length > 0) {
-//				$scope.forms.forgetPassword.successMessage = data.successMessages[0].message;
-//				$timeout(function(){ $('.bs-forget-password-modal').modal('hide'); }, 2000);
-//			}
-//		})
-//		.error(function(data, status) {
-//			if (status >= 400 && status <= 505 ) {
-//				angular.element('.bs-forget-password-modal').modal('hide');
-//				angular.element('body').removeClass('loading');
-//				angular.element('.session-expired').modal('show');
-//				$timeout(function(){
-//					window.location.href='/';
-//				},2500);
-//			}
-//		});
-//    };
+    $scope.recuperarSenha = function() {
+    	angular.element('body').addClass('loading');
+    	$scope.forms.forgetPassword.errorMessage = null;
+        $scope.forms.forgetPassword.successMessage = null;
+        $http.post('/api/perfil/recover-password?_csrf='+angular.element('#_csrf').val(),
+			{email1:$scope.forms.forgetPassword.email1, email2:$scope.forms.forgetPassword.email2}).success(function(data) {
+			angular.element('body').removeClass('loading');
+			if (data.errorMessages.length > 0) {
+				$scope.forms.forgetPassword.errorMessage = data.errorMessages[0].message;
+			}
+			if (data.successMessages.length > 0) {
+				$scope.forms.forgetPassword.successMessage = data.successMessages[0].message;
+				$timeout(function(){ $('.bs-forget-password-modal').modal('hide'); }, 2000);
+			}
+		})
+		.error(function(data, status) {
+			if (status >= 400 && status <= 505 ) {
+				angular.element('.bs-forget-password-modal').modal('hide');
+				angular.element('body').removeClass('loading');
+				angular.element('.session-expired').modal('show');
+				$timeout(function(){
+					window.location.href='/';
+				},2500);
+			}
+		});
+    };
 
-//    $scope.alterarSenha = function() {
-//    	angular.element('body').addClass('loading');
-//    	
-//    	$scope.forms.changePassword.errorMessage = null;
-//    	$scope.forms.changePassword.successMessage = null;
-//    	
-//        $http.post('/security/api/change-password?_csrf='+angular.element('#_csrf').val(),
-//            {pass1:$scope.forms.changePassword.pass1, pass2:$scope.forms.changePassword.pass2}).success(function(data) {
-//            angular.element('body').removeClass('loading');
-//            if (data.errorMessages.length > 0) {
-//                $scope.forms.changePassword.errorMessage = data.errorMessages[0].message;
-//            }
-//            if (data.successMessages.length > 0) {
-//                $scope.forms.changePassword.successMessage = data.successMessages[0].message;
-//                $timeout(function(){ $('#modalSenha').modal('hide'); }, 1500);
-//            }
-//        })
-//		.error(function(data, status) {
-//			if (status >= 400 && status <= 505 ) {
-//				angular.element('#modalSenha').modal('hide');
-//				angular.element('body').removeClass('loading');
-//				angular.element('.session-expired').modal('show');
-//				$timeout(function(){
-//					window.location.href='/';
-//				},2500);
-//			}
-//		});
-//    };
+    $scope.alterarSenha = function() {
+    	angular.element('body').addClass('loading');
+    	
+    	$scope.forms.changePassword.errorMessage = null;
+    	$scope.forms.changePassword.successMessage = null;
+    	
+        $http.post('/security/api/change-password?_csrf='+angular.element('#_csrf').val(),
+            {pass1:$scope.forms.changePassword.pass1, pass2:$scope.forms.changePassword.pass2}).success(function(data) {
+            angular.element('body').removeClass('loading');
+            if (data.errorMessages.length > 0) {
+                $scope.forms.changePassword.errorMessage = data.errorMessages[0].message;
+            }
+            if (data.successMessages.length > 0) {
+                $scope.forms.changePassword.successMessage = data.successMessages[0].message;
+                $timeout(function(){ $('#modalSenha').modal('hide'); }, 1500);
+            }
+        })
+		.error(function(data, status) {
+			if (status >= 400 && status <= 505 ) {
+				angular.element('#modalSenha').modal('hide');
+				angular.element('body').removeClass('loading');
+				angular.element('.session-expired').modal('show');
+				$timeout(function(){
+					window.location.href='/';
+				},2500);
+			}
+		});
+    };
     
     $scope.passwordErrorMessage = '';
     $scope.passwordSuccessMessage = '';
-//    
-//    $scope.changePasswordExpired = function() {
-//    	
-//        angular.element('html').addClass('loading');
-//        angular.element('#password-success').css('display','none');
-//        angular.element('#password-error').css('display','none');
-//
-//        $scope.password = new Password($scope.forms.signexpired);
-//        $scope.password.$change({_csrf : angular.element('#_csrf').val()},function(){
-//        	angular.element('html').removeClass('loading');
-//            if($scope.password.resultType == 'SUCCESS') {
-//            	$scope.passwordSuccessMessage = $scope.password.messages[0].message;
-//            	angular.element('#password-success').css('display','block');
-//            	$scope.forms.signexpired = [];
-//            	$timeout(function(){
-//					window.location.href='/';
-//				},5000);
-//            } else {
-//            	if($scope.password.errorMessages != null && $scope.password.errorMessages.length > 0) {
-//            		$scope.passwordErrorMessage = $scope.password.errorMessages[0].message;
-//            		angular.element('html').removeClass('loading');
-//        			angular.element('#password-error').css('display','block');
-//            	}
-//            }
-//        },
-//		function(data) {
-//			if (data.status >= 400 && data.status <= 505 ) {
-//				angular.element('html').removeClass('loading');
-//				angular.element('.session-expired').modal('show');
-//				$timeout(function(){
-//					window.location.href='/';
-//				},2500);
-//			}
-//		});    	
-//    	
-//    }
+    
+    $scope.changePasswordExpired = function() {
+    	
+        angular.element('html').addClass('loading');
+        angular.element('#password-success').css('display','none');
+        angular.element('#password-error').css('display','none');
 
-//    $scope.alterarSenhaAlteracaoIndevida = function(token) {
-//    	angular.element('body').addClass('loading');
-//        $scope.forms.changePassword.errorMessage = null;
-//        $scope.forms.changePassword.successMessage = null;
-//        $http.post('/api/forgot-password/' + token + '?_csrf='+angular.element('#_csrf').val(),
-//            {pass1:$scope.forms.changePassword.pass1, pass2:$scope.forms.changePassword.pass2}).success(function(data) {
-//            angular.element('body').removeClass('loading');
-//            if (data.errorMessages.length > 0) {
-//                $scope.forms.changePassword.errorMessage = data.errorMessages[0].message;
-//            }
-//            if (data.successMessages.length > 0) {
-//                $scope.forms.changePassword.successMessage = data.successMessages[0].message;
-//                $timeout(function(){
-//                    window.location.href='/';
-//                },1500);
-//            }
-//        })
-//		.error(function(data, status) {
-//			if (status >= 400 && status <= 505 ) {
-//				angular.element('#modalSenha').modal('hide');
-//				angular.element('body').removeClass('loading');
-//				angular.element('.session-expired').modal('show');
-//				$timeout(function(){
-//					window.location.href='/';
-//				},2500);
-//			}
-//		});
-//    };
+        $scope.password = new Password($scope.forms.signexpired);
+        $scope.password.$change({_csrf : angular.element('#_csrf').val()},function(){
+        	angular.element('html').removeClass('loading');
+            if($scope.password.resultType == 'SUCCESS') {
+            	$scope.passwordSuccessMessage = $scope.password.messages[0].message;
+            	angular.element('#password-success').css('display','block');
+            	$scope.forms.signexpired = [];
+            	$timeout(function(){
+					window.location.href='/';
+				},5000);
+            } else {
+            	if($scope.password.errorMessages != null && $scope.password.errorMessages.length > 0) {
+            		$scope.passwordErrorMessage = $scope.password.errorMessages[0].message;
+            		angular.element('html').removeClass('loading');
+        			angular.element('#password-error').css('display','block');
+            	}
+            }
+        },
+		function(data) {
+			if (data.status >= 400 && data.status <= 505 ) {
+				angular.element('html').removeClass('loading');
+				angular.element('.session-expired').modal('show');
+				$timeout(function(){
+					window.location.href='/';
+				},2500);
+			}
+		});    	
+    	
+    }
+
+    $scope.alterarSenhaAlteracaoIndevida = function(token) {
+    	angular.element('body').addClass('loading');
+        $scope.forms.changePassword.errorMessage = null;
+        $scope.forms.changePassword.successMessage = null;
+        $http.post('/api/forgot-password/' + token + '?_csrf='+angular.element('#_csrf').val(),
+            {pass1:$scope.forms.changePassword.pass1, pass2:$scope.forms.changePassword.pass2}).success(function(data) {
+            angular.element('body').removeClass('loading');
+            if (data.errorMessages.length > 0) {
+                $scope.forms.changePassword.errorMessage = data.errorMessages[0].message;
+            }
+            if (data.successMessages.length > 0) {
+                $scope.forms.changePassword.successMessage = data.successMessages[0].message;
+                $timeout(function(){
+                    window.location.href='/';
+                },1500);
+            }
+        })
+		.error(function(data, status) {
+			if (status >= 400 && status <= 505 ) {
+				angular.element('#modalSenha').modal('hide');
+				angular.element('body').removeClass('loading');
+				angular.element('.session-expired').modal('show');
+				$timeout(function(){
+					window.location.href='/';
+				},2500);
+			}
+		});
+    };
 
     $scope.buscar = function(sumario) {
     	var q = sumario.tags[0].descricao;
@@ -425,65 +425,65 @@ app.controller('SiteController', function ($scope, $http, $filter, $interval, $t
         $scope.forms.contact.waitMessage = null;
     };
 
-//    $scope.sendContact = function() {
-//        angular.element('body').addClass('loading');
-//
-//        $scope.forms.contact.waitMessage = null;
-//        $scope.forms.contact.errorMessage = null;
-//        $scope.forms.contact.successMessage = null;
-//
-//        var _csrf = angular.element('#_csrf').val();
-//
-//        $scope.result = new Contact({
-//            name : $scope.forms.contact.name,
-//            website : $scope.forms.contact.website,
-//            email : $scope.forms.contact.email,
-//            message : $scope.forms.contact.message,
-//        });
-//
-//        $scope.result.$send({_csrf : _csrf}, function(){
-//            angular.element('body').removeClass('loading');
-//
-//            if($scope.result.value == false) {
-//                $scope.forms.contact.errorMessage = $scope.result.message.split(',');
-//                console.log($scope.forms.contact.errorMessage);
-//                return;
-//            }
-//
-//            $scope.forms.contact.waitMessage = 'forms.contact.waitMessage';
-//
-//            if($scope.result.value == true) {
-//                $scope.forms.contact.waitMessage = null;
-//                return;
-//            }
-//
-//            var timer = $interval(function() {
-//                $scope.result.$status(function(){
-//                    $scope.forms.contact.waitMessage = $scope.result.message;
-//                    if($scope.result.value == true || $scope.result.value == false) {
-//                        $interval.cancel(timer);
-//                    }
-//                    if($scope.result.value == false) {
-//                        $scope.forms.contact.waitMessage = null;
-//                        $scope.forms.contact.errorMessage = [];
-//                        $scope.forms.contact.errorMessage[0] = $scope.result.code + ' - ' + $scope.result.message;
-//                    }
-//                    if($scope.result.value == true) {
-//                        $scope.forms.contact.waitMessage = null;
-//                        $scope.forms.contact.successMessage = $scope.result.message;
-//                    }
-//                });
-//            }, 1000);
-//        }, function(data) {
-//			if (data.status >= 400 && data.status <= 505 ) {
-//				angular.element('body').removeClass('loading');
-//				angular.element('.session-expired').modal('show');
-//				$timeout(function(){
-//					window.location.href='/';
-//				},2500);
-//			}
-//        });
-//    };
+    $scope.sendContact = function() {
+        angular.element('body').addClass('loading');
+
+        $scope.forms.contact.waitMessage = null;
+        $scope.forms.contact.errorMessage = null;
+        $scope.forms.contact.successMessage = null;
+
+        var _csrf = angular.element('#_csrf').val();
+
+        $scope.result = new Contact({
+            name : $scope.forms.contact.name,
+            website : $scope.forms.contact.website,
+            email : $scope.forms.contact.email,
+            message : $scope.forms.contact.message,
+        });
+
+        $scope.result.$send({_csrf : _csrf}, function(){
+            angular.element('body').removeClass('loading');
+
+            if($scope.result.value == false) {
+                $scope.forms.contact.errorMessage = $scope.result.message.split(',');
+                console.log($scope.forms.contact.errorMessage);
+                return;
+            }
+
+            $scope.forms.contact.waitMessage = 'forms.contact.waitMessage';
+
+            if($scope.result.value == true) {
+                $scope.forms.contact.waitMessage = null;
+                return;
+            }
+
+            var timer = $interval(function() {
+                $scope.result.$status(function(){
+                    $scope.forms.contact.waitMessage = $scope.result.message;
+                    if($scope.result.value == true || $scope.result.value == false) {
+                        $interval.cancel(timer);
+                    }
+                    if($scope.result.value == false) {
+                        $scope.forms.contact.waitMessage = null;
+                        $scope.forms.contact.errorMessage = [];
+                        $scope.forms.contact.errorMessage[0] = $scope.result.code + ' - ' + $scope.result.message;
+                    }
+                    if($scope.result.value == true) {
+                        $scope.forms.contact.waitMessage = null;
+                        $scope.forms.contact.successMessage = $scope.result.message;
+                    }
+                });
+            }, 1000);
+        }, function(data) {
+			if (data.status >= 400 && data.status <= 505 ) {
+				angular.element('body').removeClass('loading');
+				angular.element('.session-expired').modal('show');
+				$timeout(function(){
+					window.location.href='/';
+				},2500);
+			}
+        });
+    };
 
    $scope.LoadAjaxContent('navegacao.html', 'Developer')
    

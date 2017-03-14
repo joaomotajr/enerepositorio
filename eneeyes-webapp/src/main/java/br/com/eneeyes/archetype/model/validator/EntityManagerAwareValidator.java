be0718b6c3165f459527829f770e994e4b24a799
Joao Junior <joaomotajr@hotmail.com>
2017-03-14 18:19:33 -0300

    Limpeza de Arquivos
    Limpeza de Arquivos

--- eneeyes-webapp/src/main/java/br/com/eneeyes/archetype/model/validator/EntityManagerAwareValidator.java
+++
@@ -1,7 +0,0 @@
-package br.com.eneeyes.archetype.model.validator;
-
-import javax.persistence.EntityManager;
-
-public interface EntityManagerAwareValidator {  
-    void setEntityManager(EntityManager entityManager); 
-}

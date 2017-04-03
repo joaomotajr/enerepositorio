985fa7433e5b7acc04299af3997ae278f8947c17
Joao Junior <joaomotajr@hotmail.com>
2017-04-03 18:11:41 -0300

    Ajuste da View de Detectores associados a Alarmes

--- eneeyes-webapp/src/main/java/br/com/eneeyes/main/model/views/scripts/alarm_companydetector_sensor_view.sql
+++ eneeyes-webapp/src/main/java/br/com/eneeyes/main/model/views/scripts/alarm_companydetector_sensor_view.sql
@@ -10,7 +10,9 @@ VIEW `alarm_companydetector_sensor_view` AS
         `cda`.`COMPANY_DETECTOR_ID` AS `COMPANY_DETECTOR_ID`,
         `cda`.`SENSOR_ID` AS `SENSOR_ID`,
         `cd`.`NAME` AS `COMPANY_DETECTOR_NAME`,
-        `s`.`NAME` AS `SENSOR_NAME`
+        `cd`.`LOCAL` AS `COMPANY_DETECTOR_LOCAL`,
+        `s`.`NAME` AS `SENSOR_NAME`,
+        `s`.`RANGE_MAX` AS `RANGE_MAX`
     FROM
         ((`company_detector_alarms` `cda`
         JOIN `company_detector` `cd` ON ((`cda`.`COMPANY_DETECTOR_ID` = `cd`.`UID`)))

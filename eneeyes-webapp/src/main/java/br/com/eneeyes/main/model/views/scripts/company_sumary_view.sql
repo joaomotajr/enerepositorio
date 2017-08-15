770a103e89021eff9bd0cf1d7ca65e1940e16bdc
Joao Junior <joaomotajr@hotmail.com>
2017-08-15 20:12:56 -0300

    View de Overview e Backup de views

---
+++ eneeyes-webapp/src/main/java/br/com/eneeyes/main/model/views/scripts/company_sumary_view.sql
@@ -0,0 +1,35 @@
+
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `company_sumary_view` AS
+    SELECT 
+        `a`.`UID` AS `uid`,
+        `a`.`company_name` AS `company_name`,
+        SUM((CASE
+            WHEN ISNULL(`a`.`unit_name`) THEN 0
+            ELSE 1
+        END)) AS `units`,
+        SUM((CASE
+            WHEN ISNULL(`a`.`area_name`) THEN 0
+            ELSE 1
+        END)) AS `areas`,
+        SUM(`a`.`devices`) AS `devices`
+    FROM
+        (SELECT 
+            `c`.`UID` AS `UID`,
+                `c`.`NAME` AS `company_name`,
+                `u`.`NAME` AS `unit_name`,
+                `a`.`NAME` AS `area_name`,
+                SUM((CASE
+                    WHEN ISNULL(`cd`.`NAME`) THEN 0
+                    ELSE 1
+                END)) AS `devices`
+        FROM
+            (((`enedb`.`company_view` `c`
+        LEFT JOIN `enedb`.`unit` `u` ON ((`c`.`UID` = `u`.`COMPANY_ID`)))
+        LEFT JOIN `enedb`.`area` `a` ON ((`u`.`UID` = `a`.`UNIT_ID`)))
+        LEFT JOIN `enedb`.`company_device` `cd` ON ((`a`.`UID` = `cd`.`AREA_ID`)))
+        GROUP BY `c`.`UID` , `c`.`NAME` , `u`.`UID` , `u`.`NAME` , `a`.`UID` , `a`.`NAME`) `a`
+    GROUP BY `a`.`UID`
